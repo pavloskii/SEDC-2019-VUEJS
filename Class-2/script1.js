cf0b65052959416e942b4b936216e8320ad30c08
Igor Pavloski <Igor.Pavloski@seavus.com>
2019-09-24 19:35:31 +0200

    Deleted Class 2 in the Class1 branch

--- Class-2/script1.js
+++
@@ -1,6 +0,0 @@
-// import { name, someFunc } from "./script2.js";
-import Person from "./script2";
-
-console.log(name);
-
-someFunc();
--- Class-2/script2.js
+++
@@ -1,6 +0,0 @@
-// export const name = "John";
-// export function someFunc() {
-//   console.log("od drug files");
-// }
-
-export default { name: "John", age: 123 };
