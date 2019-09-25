7324142c66f3568f983116afc980979361a0c3f3
Igor Pavloski <Igor.Pavloski@seavus.com>
2019-09-25 20:56:34 +0200

    Footerot

--- full-project/src/components/Footer.vue
+++ full-project/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <nav class="navbar fixed-bottom navbar-dark bg-primary">
-      <a class="navbar-brand">Fixed top</a>
+      <a class="navbar-brand text-light">Copyright © SEDC</a>
     </nav>
   </footer>
 </template>
